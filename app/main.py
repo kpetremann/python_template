ed6a89308bf8a3f194794fca0a832c4d48b64add
Kevin Petremann <kpetrem@gmail.com>
2020-10-17 11:43:08 +0200

    First base

---
+++ app/main.py
@@ -0,0 +1,4 @@
+"""Main code."""
+
+if __name__ == "__main__":
+    print("Hello world!")
---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""Configure all the PEX."""
+
+import os
+import setuptools  # type: ignore
+
+
+def _read_reqs(relpath):
+    fullpath = os.path.join(os.path.dirname(__file__), relpath)
+    with open(fullpath) as open_file:
+        return [s.strip() for s in open_file.readlines() if (s.strip() and not s.startswith("#"))]
+
+
+_INSTALL_REQUIRES = []
+_REQUIREMENTS_FILES = ["requirements/base.txt"]
+
+for req in _REQUIREMENTS_FILES:
+    _REQUIREMENTS_TXT = _read_reqs(req)
+    _INSTALL_REQUIRES.extend([line for line in _REQUIREMENTS_TXT if "://" not in line])
+
+setuptools.setup(
+    name="app",
+    version="0.1",
+    include_package_data=True,
+    install_requires=_INSTALL_REQUIRES,
+    tests_require=_read_reqs("requirements/tests.txt"),
+    dependency_links=[],
+    entry_points={"console_scripts": ["app = app.main:main"]},
+    packages=setuptools.find_packages(),
+)
