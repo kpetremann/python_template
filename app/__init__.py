06af51c3dc81a8ccacf99f4b78d398f9d4f40975
Kevin Petremann <kpetrem@gmail.com>
2020-11-17 13:51:28 +0100

    Add prettier stacktrace

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,8 @@
 """Main app package."""
+
+try:
+    from app import pretty
+
+    pretty.full()
+except ImportError:
+    pass
---
+++ app/pretty.py
@@ -0,0 +1,33 @@
+"""Prettier stacktrace."""
+import pretty_errors
+
+
+def simple():
+    """Set simple and pretty stacktrace."""
+    pretty_errors.configure(
+        separator_character="*",
+        filename_display=pretty_errors.FILENAME_EXTENDED,
+        line_number_first=True,
+        display_link=True,
+        line_color=pretty_errors.RED + "> " + pretty_errors.default_config.line_color,
+        code_color="  " + pretty_errors.default_config.line_color,
+        truncate_code=True,
+        display_locals=True,
+    )
+
+
+def full():
+    """Set more verbose stacktrace."""
+    pretty_errors.configure(
+        separator_character="*",
+        filename_display=pretty_errors.FILENAME_EXTENDED,
+        line_number_first=True,
+        display_link=True,
+        lines_before=5,
+        lines_after=2,
+        line_color=pretty_errors.RED + "> " + pretty_errors.default_config.line_color,
+        code_color="  " + pretty_errors.default_config.line_color,
+        truncate_code=True,
+        display_locals=True,
+        display_trace_locals=True,
+    )
